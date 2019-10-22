6f6282c13dfceb7fbdf8a3ed7667e6ada34fb434
794784 <54677860+794784@users.noreply.github.com>
2019-10-22 08:47:38 -0700

    l

--- Lab1004 mySort/sketch.js
+++ Lab1004 mySort/sketch.js
@@ -2,20 +2,13 @@
 // 	Date or version number
 //  This is a comment
 //  The setup function function is called once when your program begins
+var list = [ 5, 9, 10, 6, 2, 1, 3, 0];
+var order =[0,0,0,0,0,0,0,0];
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
-  var list = [4, 8, 6, 3, 2, 9]
-  swap(list, 1, 2)
-  function swap(list, a, b){
-    if(a > b){
-      var temp = list[a];
-      list[a] = list[b];
-      list[b] = temp;
-    }
-  }
 
 
 }
@@ -24,3 +17,22 @@ function setup() {
 function draw() {
 
 }
+
+function mySort(){
+
+var small = 0;
+var i = 0;
+while(i< list.length){
+          for (var x = 0; x<list.length; x++){
+
+              if (list[x] === small){
+                  order[x].push(small);
+                  i++;
+              }
+
+          }
+          small ++;
+
+      }
+    
+    }
