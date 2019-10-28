778f464b08d99b51073a1b6a96a71fb987341702
794784 <54677860+794784@users.noreply.github.com>
2019-10-28 08:49:47 -0700

    bars

--- Lab1023 Bars/sketch.js
+++ Lab1023 Bars/sketch.js
@@ -2,38 +2,60 @@
 // 	Date or version number
 //  This is a comment
 //  The setup function function is called once when your program begins
-var bars = [];
-var numBars, barWidth;
+var bars=[0] ;
+var barWidth, barHeight;
 
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
-
-barWidth = 20;
-numBars = width/barWidth;
-
-loadBars(numBars);
-runBars();
-myBubbleSort();
-}
-
-function draw(){
-  background(0)
-  frameRate(1);
-  runBars();
-}
-function loadBars(num){
-  for(var i = 0; i < bars.length; i++){
-    bars[i].render();
+  fill(200, 30, 150);
+  loadBars();
+  frameRate(10);
+  for (var i = 0; i<bars.length; i++){
+    bars[i].run();
   }
 }
-function printRings(){
-  var circLoc = createVector(width/2, height-250);
-  var 1 = bar.length;
-  for(var i = 0; i < bars.length; i++)
-     strokeWeight(barWidth/11);
-     fill(0, 0, bars[i].b)
-     stroke(color(0,0, bars[i].b));
+
+function draw() {
+selectionSort(bars);
 }
-© 2019 GitHub, Inc.
+
+function loadBars(){
+
+  for (var i = 0; i< 40; i++){
+    barWidth = (width/40);
+    barHeight = Math.floor(random(1, height));
+    bars[i]= new Bar(i*barWidth, height-barHeight, barWidth, barHeight);
+  }
+}
+
+function  update(){
+  for(var i=0; i<bars.length; i++){
+    bars[i].set(i);
+  }
+  background(0);
+  for (var i =0; i<bars.length; i++){
+    bars[i].run();
+  }
+}
+
+function selectionSort(arr){
+  for(var i = 0; i < arr.length-1; i ++){ //moves end limit
+      var small = i;
+  for (var j = i+1 ; j < arr.length ; j++){
+      if(arr[j].getHeight() < arr[small].getHeight()){
+          small = j;
+          }
+        }
+        swap(arr, i , small);
+        update();
+      }
+
+  }
+
+function swap(arr, a, b){ //swap
+    var temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+}
